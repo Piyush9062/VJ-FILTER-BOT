285e7d8a2f0db46345608f7d0c8800ad33106445
Piyush9062 <piyushpushpad255@gmail.com>
2025-10-09 23:16:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '20713353'))
 API_HASH = environ.get('API_HASH', 'ed4d76e20f4e1bef6e834999547fd22e')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8057929734:AAFtYVF3TM8LHdye_aFufsE9pl0wMNMJHNc")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -196,4 +196,5 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
